2c485f37927890fd380b16188728c0d2e724bd23
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-25 09:58:23 -0500

    finished cats exercise

---
+++ cats-exercises/scripts/cats.js
@@ -0,0 +1,35 @@
+const cats = [
+    {
+      name: "Henry",
+      bio: "Eat plants, meow, and throw up because I ate plants. Knock over christmas tree, stare at imaginary bug, and meow and walk away. Lick master's hand at first then bite because im moody purrrrrr do not try to mix old food with new one to fool me!"
+    },
+    {
+      name: "Jessica",
+      bio: "Spend all night ensuring people don't sleep. Sleep all day. Meowzer for when in doubt, destroy the couch."
+    },
+    {
+      name: "Jake",
+      bio: "Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked munch on tasty moths for meow meow mama meow all night murr I hate humans they are so annoying."
+    }
+  ]
+  // Write out a brief list of steps needed to display this data in the DOM.
+
+  /*
+    Add an output container with an id or class to the HTML file
+    Use querySelector to get a reference to that container
+    Iterate through the data
+    Create a variable function that returns a string of HTML to put in the DOM
+    Render the HTML in the DOM
+  */
+
+const catOutputContainer = document.querySelector("#cat-bios")
+
+const catBioStringGenerator = (cat) => {
+    return `
+    <h1>${cat.name}</h1>
+    <p>${cat.bio}</p>`
+}
+
+cats.forEach(cat => {
+    catOutputContainer.innerHTML += catBioStringGenerator(cat)
+})
--- data-structures/scripts/structures.js
+++ data-structures/scripts/structures.js
@@ -4,19 +4,29 @@
 // 2. An object
 // 3. An array
 
-"lunch"
-"peanut butter and jelly"
-"whole wheat"
-"grape juice"
-"apple"
+// "lunch"
+// "peanut butter and jelly"
+// "whole wheat"
+// "grape juice"
+// "apple"
 
-const lunch = "peanut butter and jelly sandwich on whole wheat with grape juice and an apple"
+// const lunch = "peanut butter and jelly sandwich on whole wheat with grape juice and an apple"
 
-const lunch = {
-    sandwich: "peanut butter and jelly",
-    bread: "whole wheat",
-    drink: "grape juice",
-    sideItem: "apple"
-}
+// const lunch = {
+//     sandwich: "peanut butter and jelly",
+//     bread: "whole wheat",
+//     drink: "grape juice",
+//     sideItem: "apple"
+// }
 
-const lunch = ["peanut butter and jelly", "whole wheat", "grape juice", "apple"]
+// const lunch = ["peanut butter and jelly", "whole wheat", "grape juice", "apple"]
+
+// Stretch goal: Try out a relatively new feature of JavaScript called a Set(). Add each of those strings to a Set and then console.log() the Set. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+const lunchSet = new Set()
+lunchSet.add("peanut butter and jelly")
+lunchSet.add("whole wheat")
+lunchSet.add("grape juice")
+lunchSet.add("apple")
+
+console.log(lunchSet)
